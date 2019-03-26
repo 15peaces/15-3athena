2b3ee0a0c04734961114c4de407050c90ddf9961
15peaces <admin@15peaces.com>
2019-03-26 19:48:38 +0100

    == Arch Bishop and more == =General: *Merged missing changes from 3ceam rev. 661
    =Scripts:
    *Added full support to Para_Team_Mark_ in Eden Group.
    -Follow up @3760838 .
    -Thanks to @rathena .
    
    =Skills:
    *Arch Bishop 2011 update has been applied.
    -Some updates didnt make it in yet.
    
    *AB_ADORAMUS
    -Status chance code changed to official and is now affected by skill level and job level.
    
    *AB_CLEMENTIA
    -STR, INT, and DEX increase now affected by AL_BLESSING level learned and caster's job level.
    
    *AB_CANTO
    -AGI increase now affected by AL_INCAGI level learned and caster's job level.
    
    *AB_EPICLESIS
    -HP/SP recovery is changed from every 1 second to every 3 seconds.
    
    *AB_LAUDAAGNUS
    -Increased status recovery chance.
    -Now recovers from Frozen, Petrification, Blind, Freezing, Crystallization, and Burning
    
    *AB_RENOVATIO
    -Damage formula on undead monsters is now official.
    -Supposed to be applied as a debuff on undead monsters and deal damage every 5 seconds, but will require a partal recode to allow this function.
    
    *AB_HIGHNESSHEAL
    -Heal modifier code upgraded to a full percentage range to ensure full accurate heal amount increase.
    
    *AB_CLEARANCE
    -Increased the success chance.
    
    *AB_EXPIATIO
    -Fixed a issue where it would enchant the player's weapon with holy element.
    -Their's no info anywhere saying it enchants the player's weapon with holy element.
    -It only allows you to bypass a player's defense.

--- src/map/skill.c
+++ src/map/skill.c
@@ -376,7 +376,7 @@ int skill_calc_heal(struct block_list *src, struct block_list *target, int skill
 	}
 
 	if( skill_id == AB_HIGHNESSHEAL )
-		hp = (hp * (20 + 3 * (skill_lv - 1))) / 10;
+		hp = (hp * (200 + 30 * (skill_lv - 1))) / 100;
 
 	if( ( (target && target->type == BL_MER) || !heal ) && skill_id != NPC_EVILLAND )
 		hp >>= 1;
@@ -1061,7 +1061,7 @@ int skill_additional_effect (struct block_list* src, struct block_list *bl, int 
 		break;
 	case AB_ADORAMUS:
 		if( tsc && !tsc->data[SC_DECREASEAGI] ) //Prevent duplicate agi-down effect.
-			sc_start(bl, SC_ADORAMUS, 100, skilllv, skill_get_time(skillid, skilllv));
+			sc_start(bl, SC_ADORAMUS, 4 * skilllv + sd->status.job_level / 2, skilllv, skill_get_time(skillid, skilllv));
 		break;
 	case WL_CRIMSONROCK:
 		sc_start(bl, SC_STUN, 40, skilllv, skill_get_time(skillid, skilllv));
@@ -7355,8 +7355,8 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 	case AB_CLEMENTIA:
 	case AB_CANTO:
 		{
-			int bless_lv = pc_checkskill(sd,AL_BLESSING);
-			int agi_lv = pc_checkskill(sd,AL_INCAGI);
+			int bless_lv = pc_checkskill(sd, AL_BLESSING) + sd->status.job_level / 10;
+			int agi_lv = pc_checkskill(sd, AL_INCAGI) + sd->status.job_level / 10;
 			if( sd == NULL || sd->status.party_id == 0 || (flag & 1) )
 				clif_skill_nodamage(bl, bl, skillid, skilllv, sc_start(bl,type,100,
 					(skillid == AB_CLEMENTIA)? bless_lv : (skillid == AB_CANTO)? agi_lv : skilllv,skill_get_time(skillid,skilllv)));
@@ -7397,11 +7397,16 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 
 	case AB_LAUDAAGNUS:
 		if( flag&1 || sd == NULL ) {
-			if( (tsc && (tsc->data[SC_FREEZE] || tsc->data[SC_STONE] ||
-				tsc->data[SC_BLIND]))&& (rand()%100 < 30+5*skilllv) ) {
-				status_change_end(bl, SC_FREEZE, INVALID_TIMER);
+			if ((tsc && (tsc->data[SC_STONE] || tsc->data[SC_FREEZE] ||
+				tsc->data[SC_BLIND] || tsc->data[SC_BURNING] ||
+				tsc->data[SC_FREEZING] || tsc->data[SC_CRYSTALIZE])) && (rand() % 100 < 40 + 10 * skilllv))
+			{
 				status_change_end(bl, SC_STONE, INVALID_TIMER);
+				status_change_end(bl, SC_FREEZE, INVALID_TIMER);
 				status_change_end(bl, SC_BLIND, INVALID_TIMER);
+				status_change_end(bl, SC_BURNING, INVALID_TIMER);
+				status_change_end(bl, SC_FREEZING, INVALID_TIMER);
+				status_change_end(bl, SC_CRYSTALIZE, INVALID_TIMER);
 			}
 			// Success rate only applies to the curing effect and not stat bonus.
 			clif_skill_nodamage(bl, bl, skillid, skilllv,
@@ -7446,7 +7451,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 		if (flag&1 || (i = skill_get_splash(skillid, skilllv)) < 1)
 		{ //As of the behavior in official server Clearance is just a super version of Dispell skill. [Jobbie]
 			clif_skill_nodamage(src,bl,skillid,skilllv,1);
-			if((dstsd && (dstsd->class_&MAPID_UPPERMASK) == MAPID_SOUL_LINKER) || rand()%100 >= 30 + 10 * skilllv)
+			if((dstsd && (dstsd->class_&MAPID_UPPERMASK) == MAPID_SOUL_LINKER) || rand()%100 >= 60 + 8 * skilllv)
 			{
 				if (sd)
 					clif_skill_fail(sd,skillid,0,0,0);
